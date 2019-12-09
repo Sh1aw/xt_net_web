102029c01f160e51a20a56096396fc45cbfef28e
Sh1aw <viktornforce@gmail.com>
2019-12-09 15:10:27 +0400

    Add Task 3.1, Task 3.2 and part of Task 3.3

---
+++ Task_03/DynamicArray/DynamicArray.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03.DynamicArray
+{
+    class DynamicArray<T>
+    {
+        public int Length { get; private set; }
+        public int Capacity
+        {
+            get => array.Length;
+        }
+
+        private T[] array;
+        public DynamicArray()
+        {
+            array = new T[8];
+            Length = 0;
+        }
+        public DynamicArray(int size)
+        {
+            array = new T[size];
+            Length = 0;
+        }
+        public DynamicArray(IEnumerable<T> coll)
+        {
+            array = new T[coll.Count()];
+            array = coll.ToArray();
+            Length = coll.Count();
+
+        }
+
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index > Length || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return array[index];
+            }
+            set
+            {
+                if (index > Length || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                array[index] = value;
+            }
+        }
+        public void Add(T something)
+        {
+            if (Length >= Capacity)
+            {
+                ResizeArray();
+            }
+            array[Length] = something;
+            Length++;
+        }
+        public void AddRange(IEnumerable<T> enums)
+        {
+            if ((Length + enums.Count()) >= Capacity)
+            {
+                ResizeArray(Length + enums.Count());
+            }
+            for (int i = 0; i < enums.Count(); i++)
+            {
+                array[Length + i] = enums.ToArray()[i];
+            }
+            Length = Length + enums.Count();
+        }
+        public bool Remove(T elem)
+        {
+            int index = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Equals(elem))
+                {
+                    index = i;
+                    MakeUnShift(index);
+                    Length--;
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool Insert(int position, T elem)
+        {
+            if (position < 0 || position > Length)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if ((Length + 1) >= Capacity)
+            {
+                ResizeArray();
+            }
+            MakeShift(position, elem);
+            Length++;
+            return true;
+
+        }
+        public T[] ToArray()
+        {
+            T[] outArray = new T[Length];
+            for (int i = 0; i < Length; i++)
+            {
+                outArray[i] = array[i];
+            }
+            return outArray;
+        }
+
+        private void MakeUnShift(int index)
+        {
+            for (int i = index; i < Length; i++)
+            {
+                array[i] = array[i + 1];
+            }
+        }
+        private void MakeShift(int index, T elem)
+        {
+            for (int i = Length; i >= index; i--)
+            {
+                array[i + 1] = array[i];
+            }
+            array[index] = elem;
+        }
+        private void ResizeArray()
+        {
+            T[] newArray = new T[array.Length * 2];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
+        private void ResizeArray(int newCapacity)
+        {
+            T[] newArray = new T[newCapacity];
+            int iU = 0;
+            if (newCapacity <= array.Length)
+            {
+                iU = newCapacity;
+            }
+            else
+            {
+                iU = array.Length;
+            }
+            for (int i = 0; i < iU; i++)
+            {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
+
+    }
+}
---
+++ Task_03/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 3.1
+            Lost();
+            Console.WriteLine();
+            //Task 3.2
+            WordFrequency();
+            Console.ReadKey();
+
+        }
+
+        //Task 3.1
+
+        public static void Lost()
+        {
+            Console.WriteLine("\nTask 3.1 Lost:");
+            List<String> list = new List<String>() { "1st.", "2nd.", "3rd.", "4th.", "5th.", "6th.", "7th.", "8th.", "9th." };
+            Console.WriteLine("Starting items: ");
+            OutList(list);
+            Console.Write("\nLast is: ");
+            OutList(DeleteEverySecond(list));
+        }
+        public static List<T> DeleteEverySecond<T>(List<T> list)
+        {
+
+            int count = 1;
+            while (list.Count() > 1)
+            {
+                List<T> temp = new List<T>();
+                foreach (var item in list)
+                {
+                    if (count % 2 != 0)
+                    {
+                        temp.Add(item);
+                    }
+                    count++;
+                }
+                list = temp;
+
+            }
+            return list;
+        }
+        public static void OutList<T>(List<T> list)
+        {
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+
+        //Task 3.2
+
+        public static void WordFrequency()
+        {
+            Console.WriteLine("\nTask 3.2 WordFrequency:");
+            Console.WriteLine("[Word] : [Frequency]");
+            String englishText = "The announcement did not include any specific" +
+                " titles or dates to look forward to, " +
+                "but did state that “multiple titles” were in development." +
+                " There was also no confirmation about which developers will be working with Riot Forge, though that information" +
+                " will likely come in the future, along with game announcements." +
+                " The first game will be announced at The Game Awards on Dec. 12";
+            OutDictionary(GetWordStats(englishText));
+
+
+        }
+        public static Dictionary<String, int> GetWordStats(String englishText)
+        {
+            Dictionary<string, int> wordStats = new Dictionary<string, int>();
+            StringBuilder cleanText = new StringBuilder();
+
+            for (int i = 0; i < englishText.Length; i++)
+            {
+                if (englishText[i] == '.')
+                {
+                    cleanText.Append(englishText[i]);
+                }
+                else if (Char.IsPunctuation(englishText[i]))
+                {
+
+                }
+                else
+                {
+                    cleanText.Append(englishText[i]);
+                }
+            }
+
+            var everyWord = cleanText.ToString().Split(' ', '.');
+            for (int i = 0; i < everyWord.Length; i++)
+            {
+                int wordCounter = 0;
+                for (int j = 0; j < everyWord.Length; j++)
+                {
+                    if (everyWord[j].ToLower() == everyWord[i].ToLower())
+                    {
+                        wordCounter++;
+                    }
+                }
+                if (!wordStats.ContainsKey(everyWord[i].ToLower()) && (!String.IsNullOrEmpty(everyWord[i])))
+                {
+                    wordStats.Add(everyWord[i].ToLower(), wordCounter);
+                }
+            }
+            return wordStats;
+
+        }
+        public static void OutDictionary(Dictionary<String, int> dictionary)
+        {
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine("{0} : {1}", item.Key, item.Value);
+            }
+        }
+    }
+}
