e108be55c9cff8f9e3c75f29618c2069363c6add
Sh1aw <viktornforce@gmail.com>
2019-12-23 17:45:07 +0400

    Add Task 5

---
+++ Task_05/MyFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_05
+{
+    class MyFile
+    {
+        public string Name { get; private set; }
+        public DateTime TimeOfCreation { get; set; } // cant desirialize json, with private setter of time-prop
+        public string Value { get; private set; }
+        public string Reason { get; private set; }
+        public string FullPath { get; private set; }
+        public string OldPath { get; private set; }
+        public string OldName { get; private set; }
+
+        public MyFile(string name, string value, DateTime time, string reason, string fullPath, string oldName, string oldPath)
+        {
+            Name = name;
+            Value = value;
+            TimeOfCreation = time;
+            Reason = reason;
+            FullPath = fullPath;
+            OldPath = oldPath;
+            OldName = oldName;
+        }
+        public override string ToString()
+        {
+            return String.Format("Name: {0}; Date: {1}; Value {2}; Reason: {3}; FullPath: {4}; OldName: {5}", Name, TimeOfCreation, Value, Reason, FullPath, OldName);
+        }
+    }
+}
---
+++ Task_05/Program.cs
@@ -0,0 +1,196 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_05
+{
+    class Program
+    {
+        const string DIR = "MyGit";
+        const string LOG_FILE = "log.json";
+        static List<MyFile> logList = new List<MyFile>();
+
+        static void Main(string[] args)
+        {
+            if (!Directory.Exists(DIR))
+            {
+                Directory.CreateDirectory(DIR);
+            }
+
+            FileSystemWatcher fw = new FileSystemWatcher(DIR, "*.txt");
+            fw.EnableRaisingEvents = true;
+            fw.IncludeSubdirectories = true;
+            fw.Deleted += new FileSystemEventHandler(UnicFileHandler);
+            fw.Created += new FileSystemEventHandler(UnicFileHandler);
+            fw.Changed += new FileSystemEventHandler(UnicFileHandler);
+            fw.Renamed += new RenamedEventHandler(RenamedFileHandler);
+
+            Console.WriteLine("Select app mode:\n1." +
+                " Track changes in target directory;\n2." +
+                " Restore directory-state by date and time;\n[1/2]\n");
+            int UserChoice = GetUserChoiceInput();
+            switch (UserChoice)
+            {
+                case 1:
+                    {
+                        Watching();
+                        break;
+                    }
+                case 2:
+                    {
+                        BackUp();
+                        break;
+                    }
+            }
+
+        }
+
+
+        static void Watching()
+        {
+            Console.WriteLine("App in track mode now!...." +
+                                "\nTo exit tracking mode(and application), press Escape");
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            { }
+            ReWriteLog();
+        }
+
+        static void BackUp()
+        {
+            using (StreamReader sr = new StreamReader(LOG_FILE))
+            {
+                logList = new List<MyFile>(JsonConvert.DeserializeObject<List<MyFile>>(sr.ReadToEnd()));
+            }
+            Console.WriteLine("App in BackUp mode now!");
+            while (true)
+            {
+                Console.WriteLine("\nEnter target date and time!" +
+                                " Target date format is: dd\\mm\\yyyy hh:mm:ss");
+                DateTime dataInput = GetDataUserInput();
+
+                List<MyFile> OutlogList = new List<MyFile>();
+                FileTools.ClearDir(DIR);
+
+                lock (logList)
+                {
+                    OutlogList = logList.Where(x => x.TimeOfCreation < dataInput).ToList();
+                }
+                FileTools.GetLastStateFileHandler(OutlogList, FileTools.GetLastStateFile);
+
+                var newListGroups = OutlogList.GroupBy(x => x.Name);
+                foreach (var item in newListGroups)
+                {
+                    var temp1 = item.LastOrDefault();
+                    if (temp1.Reason != "Deleted")
+                    {
+                        FileTools.CreateStateFile(temp1);
+                    }
+                }
+                using (StreamWriter w = new StreamWriter(LOG_FILE, false))
+                {
+                    w.WriteLine(JsonConvert.SerializeObject(logList, Formatting.Indented));
+                }
+                Console.WriteLine("BackUp was done! Current directory state: " + dataInput);
+            }
+        }
+
+        
+
+        static void UnicFileHandler(object sender, FileSystemEventArgs args)
+        {
+            string value = "";
+            if (args.ChangeType.ToString() == "Changed" || args.ChangeType.ToString() == "Created")
+            {
+                value = FileTools.ReadNewValue(args.FullPath);
+            }
+            lock (logList)
+            {
+                logList.Add(new MyFile(args.Name, value, DateTime.Now, args.ChangeType.ToString(), args.FullPath, args.Name, args.FullPath));
+            }
+        }
+        static void RenamedFileHandler(object sender, RenamedEventArgs args)
+        {
+            lock (logList)
+            {
+                logList.Add(new MyFile(args.Name, FileTools.ReadNewValue(args.FullPath), DateTime.Now, args.ChangeType.ToString(), args.FullPath, args.OldName, args.OldFullPath));
+            }
+        }
+
+
+        //Append new part of logs in log file
+        public static void ReWriteLog()
+        {
+            List<MyFile> tempList = new List<MyFile>();
+            using (StreamReader sr = new StreamReader(LOG_FILE))
+            {
+                string s = sr.ReadToEnd();
+                if (s.Length != 0)
+                {
+                    tempList = new List<MyFile>(JsonConvert.DeserializeObject<List<MyFile>>(s));
+                    lock (logList)
+                    {
+                        foreach (var item in logList)
+                        {
+                            tempList.Add(item);
+                        }
+                    }
+                }
+            }
+            using (StreamWriter w = new StreamWriter(LOG_FILE, append: false))
+            {
+                if (tempList.Count > 0)
+                {
+                    w.Write(JsonConvert.SerializeObject(tempList, Formatting.Indented));
+                }
+                else
+                {
+                    w.Write(JsonConvert.SerializeObject(logList, Formatting.Indented));
+                }
+
+            }
+        }
+
+        static DateTime GetDataUserInput()
+        {
+            DateTime date = new DateTime();
+            bool flag = true;
+            while (flag)
+            {
+                string stringData = Console.ReadLine();
+                if (DateTime.TryParse(stringData, out date))
+                {
+                    flag = false;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input! Try again and follow this date format: dd/mm/yyyy hh:mm:ss");
+                }
+            }
+            return date;
+        }
+        static int GetUserChoiceInput()
+        {
+            int choice = 0;
+            bool flag = true;
+            while (flag)
+            {
+                string stringData = Console.ReadLine();
+                if (int.TryParse(stringData, out choice) && (choice == 1 || choice == 2))
+                {
+
+                    flag = false;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input! Please try again");
+                }
+            }
+            return choice;
+        }
+    }
+}
